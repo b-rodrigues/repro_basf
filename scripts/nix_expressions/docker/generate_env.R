1212e29136e9e2d8928daf2c2385213c6e0a4647
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-30 14:14:53 +0200

    content

--- scripts/nix_expressions/docker/generate_env.R
+++ scripts/nix_expressions/docker/generate_env.R
@@ -1,8 +1,13 @@
 library(rix)
 
-rix(r_ver = "4.3.1",
-    r_pkgs = c("dplyr", "ggplot2"),
-    ide = "other",
-    project_path = ".",
-	shell_hook = "R",
-    overwrite = TRUE)
+rix(
+  date = "2025-04-29",
+  r_pkgs = "ggplot2",
+  py_conf = list(
+    py_version = "3.12",
+    py_pkgs = c("polars", "great-tables")
+  ),
+  ide = "positron",
+  project_path = ".",
+  overwrite = TRUE
+)
